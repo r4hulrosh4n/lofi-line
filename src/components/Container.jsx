f576d6e742fc635e741c49318984578e5d2d2c59
Rahul Roshan <r4hulrosh4n@gmail.com>
2024-09-05 20:56:05 +0530

    player

--- src/components/Container.jsx
+++ src/components/Container.jsx
@@ -1,8 +1,9 @@
-import React from "react";
+// import React, { useState } from "react";
 import TopUi from "./TopUi";
-import BottomUi from "./BottomUi";
+// import BottomUi from "./BottomUi";
 
 const Container = () => {
+
   return (
     <div className="w-[95%] h-[95%] absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 flex flex-col">
       <TopUi />
